54dd148ae3851bbb293a90a15782c2fc4816a4b6
Cecchy2 <dariocecchinato@gmail.com>
2024-09-23 20:21:39 +0200

    aggiunto metodo get per tutti gli utenti

--- src/main/java/epic_team6/buildweek_epic_energy_services/controllers/UtentiController.java
+++ src/main/java/epic_team6/buildweek_epic_energy_services/controllers/UtentiController.java
@@ -1,15 +1,19 @@
 package epic_team6.buildweek_epic_energy_services.controllers;
 
+import epic_team6.buildweek_epic_energy_services.entities.Utente;
 import epic_team6.buildweek_epic_energy_services.exceptions.BadRequestException;
+import epic_team6.buildweek_epic_energy_services.payloads.FatturaRespDTO;
 import epic_team6.buildweek_epic_energy_services.payloads.UtentePayloadDTO;
 import epic_team6.buildweek_epic_energy_services.payloads.UtenteResponseDTO;
 import epic_team6.buildweek_epic_energy_services.services.UtenteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
@@ -29,5 +33,12 @@ public class UtentiController {
         }else{
             return new UtenteResponseDTO(this.utenteService.saveUtente(body).getId());
         }
+    }
+
+    @GetMapping
+    public Page<Utente> findAll(@RequestParam(defaultValue = "0") int page,
+                                        @RequestParam(defaultValue = "15") int size,
+                                        @RequestParam(defaultValue = "id") String sortBy) {
+        return this.utenteService.findAll(page, size, sortBy);
     }
 }
--- src/main/java/epic_team6/buildweek_epic_energy_services/services/UtenteService.java
+++ src/main/java/epic_team6/buildweek_epic_energy_services/services/UtenteService.java
@@ -1,11 +1,16 @@
 package epic_team6.buildweek_epic_energy_services.services;
 
+import epic_team6.buildweek_epic_energy_services.entities.Provincia;
 import epic_team6.buildweek_epic_energy_services.entities.Utente;
 import epic_team6.buildweek_epic_energy_services.exceptions.BadRequestException;
 import epic_team6.buildweek_epic_energy_services.exceptions.NotFoundException;
 import epic_team6.buildweek_epic_energy_services.payloads.UtentePayloadDTO;
 import epic_team6.buildweek_epic_energy_services.repositories.UtenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -14,6 +19,12 @@ import java.util.UUID;
 public class UtenteService {
     @Autowired
     private UtenteRepository utenteRepository;
+
+    public Page<Utente> findAll(int page, int size, String sortBy) {
+        if (page > 100) page = 100;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return this.utenteRepository.findAll(pageable);
+    }
 
     public Utente findUtenteById(UUID utenteId){
         Utente found = this.utenteRepository.findById(utenteId).orElseThrow(()->new NotFoundException(utenteId));
