a1c8e4bed2295cc2506dcbd37a076c6306863dff
Kenny Boateng <kennyboateng.99@gmail.com>
2024-09-23 18:37:22 +0200

    correzione ClienteDTO

--- src/main/java/epic_team6/buildweek_epic_energy_services/controllers/ClienteController.java
+++ src/main/java/epic_team6/buildweek_epic_energy_services/controllers/ClienteController.java
@@ -1,13 +1,19 @@
 package epic_team6.buildweek_epic_energy_services.controllers;
 
 import epic_team6.buildweek_epic_energy_services.entities.Cliente;
+import epic_team6.buildweek_epic_energy_services.payloads.ClienteDTO;
 import epic_team6.buildweek_epic_energy_services.services.ClienteService;
+import jakarta.validation.Valid;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/clienti")
@@ -31,8 +37,20 @@ public class ClienteController {
     // Crea un nuovo cliente
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente createCliente(@RequestBody Cliente cliente) {
-        return clienteService.salvaCliente(cliente);
+    public ClienteRespDTO createCliente(@RequestBody @Validated ClienteDTO body, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            String messages = validationResult.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.joining(". "));
+            throw new BadRequestException("Ci sono stati errori nel payload: " + messages);
+        }
+        return new ClienteRespDTO(clienteService.salvaCliente(body).getId());
+    }
+
+    // Aggionamento cliente
+    @PutMapping("/{id}")
+    public Cliente updateCliente(@PathVariable UUID id, @RequestBody @Valid ClienteDTO body) {
+        return clienteService.aggiornaESalvaCliente(id, body);
     }
 
     // Elimina un cliente per ID
@@ -42,3 +60,4 @@ public class ClienteController {
         clienteService.cancellaClienteById(id);
     }
 }
+
--- src/main/java/epic_team6/buildweek_epic_energy_services/entities/Cliente.java
+++ src/main/java/epic_team6/buildweek_epic_energy_services/entities/Cliente.java
@@ -2,17 +2,15 @@ package epic_team6.buildweek_epic_energy_services.entities;
 
 import epic_team6.buildweek_epic_energy_services.enums.TipologiaCliente;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
@@ -22,57 +20,60 @@ public class Cliente {
     @Id
     @GeneratedValue
     private UUID id;
-
     @Column
     private String ragioneSociale;
-
     @Column
     private String partitaIva;
-
     @Column
     private String email;
-
     @Column
-    private Date dataInserimento;
-
+    private LocalDate dataInserimento;
     @Column
-    private Date dataUltimoContatto;
-
+    private LocalDate dataUltimoContatto;
     @Column
     private double fatturatoAnnuale;
-
     @Column
     private String pec;
-
     @Column
     private String telefono;
-
     @Column
     private String emailContatto;
-
     @Column
     private String nomeContatto;
-
     @Column
     private String cognomeContatto;
-
     @Column
     private String telefonoContatto;
-
     @Column
     private String logoAziendale;
-
     @Enumerated(EnumType.STRING)
     @Column
     private TipologiaCliente tipologia;
-
     @Column
     private String indirizzoSedeLegale;
-
     @Column
     private String indirizzoSedeOperativa;
-
     @OneToMany(mappedBy = "cliente")
     private List<Fattura> fatture;
+
+    public Cliente(String ragioneSociale, String partitaIva, String email, LocalDate dataInserimento, LocalDate dataUltimoContatto, double fatturatoAnnuale, String pec, String telefono, String emailContatto, String nomeContatto, String cognomeContatto, String telefonoContatto, String logoAziendale, TipologiaCliente tipologia, String indirizzoSedeLegale, String indirizzoSedeOperativa) {
+        this.ragioneSociale = ragioneSociale;
+        this.partitaIva = partitaIva;
+        this.email = email;
+        this.dataInserimento = dataInserimento;
+        this.dataUltimoContatto = dataUltimoContatto;
+        this.fatturatoAnnuale = fatturatoAnnuale;
+        this.pec = pec;
+        this.telefono = telefono;
+        this.emailContatto = emailContatto;
+        this.nomeContatto = nomeContatto;
+        this.cognomeContatto = cognomeContatto;
+        this.telefonoContatto = telefonoContatto;
+        this.logoAziendale = logoAziendale;
+        this.tipologia = tipologia;
+        this.indirizzoSedeLegale = indirizzoSedeLegale;
+        this.indirizzoSedeOperativa = indirizzoSedeOperativa;
+
+    }
 
 }
