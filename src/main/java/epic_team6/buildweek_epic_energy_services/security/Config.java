25e5e9162d16c6765b5f0503d6c0e632e8aa7128
Cecchy2 <dariocecchinato@gmail.com>
2024-09-23 10:30:59 +0200

    aggiunto security package con file Config.

---
+++ src/main/java/epic_team6/buildweek_epic_energy_services/security/Config.java
@@ -0,0 +1,19 @@
+package epic_team6.buildweek_epic_energy_services.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class Config {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.formLogin(http -> http.disable());
+        httpSecurity.csrf(http -> http.disable());
+        httpSecurity.sessionManagement(http -> http.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.authorizeHttpRequests(http -> http.requestMatchers("/**").permitAll());
+        return httpSecurity.build();
+    }
+}
